6b98b1f79f186465311a80208a89bf08d9801c78
Daniel Su <danielsu@protonmail.ch>
2019-02-12 14:39:49 -0800

    add button displaying new divs in container but not the correct format

--- app/app.js
+++ app/app.js
@@ -5,6 +5,8 @@ $('.btn-addtask').on('click', function(c) {
   var id = $(".task-input").val();
   var task = $(".task-input").val();
   localStorage.setItem(id, task);
+  var displayTask = localStorage.getItem(task);
+  $(".main-container").append('<div class="display-data-item" data-task="' + task + '">' + task + '</div>');
   $(".task-input").val("");
   });
 });
@@ -15,23 +17,23 @@ $('.btn-addtask').on('click', function(c) {
 
 
 
-//   $('.btn-add').on('click', function(e){
-//     console.log(e);
-//     var keyData = $('.input-key').val();
-//     var valueData = $('.input-value').val();
-//     // write to db
-//     localStorage.setItem(keyData, valueData);
-//     // read from db
-//     var displayText = keyData + ' | ' + localStorage.getItem(keyData);
-//     // this only displays the last one? might want to switch to html
-//     // and append a div
-//     // <div class="display-data-item" data-keyValue="keyData">valueData</div>
-//     // if you use backticks ` you can use ${templateLiterals}
-//     // TODO make this vars make sense across the app
-//     $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
-//     $('.input-key').val('');
-//     $('.input-value').val('');
-//   });
+  // $('.btn-add').on('click', function(e){
+  //   console.log(e);
+  //   var keyData = $('.input-key').val();
+  //   var valueData = $('.input-value').val();
+  //   // write to db
+  //   localStorage.setItem(keyData, valueData);
+  //   // read from db
+  //   var displayText = keyData + ' | ' + localStorage.getItem(keyData);
+  //   // this only displays the last one? might want to switch to html
+  //   // and append a div
+  //   // <div class="display-data-item" data-keyValue="keyData">valueData</div>
+  //   // if you use backticks ` you can use ${templateLiterals}
+  //   // TODO make this vars make sense across the app
+  //   $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
+  //   $('.input-key').val('');
+  //   $('.input-value').val('');
+  // });
 
 
 //   // update db
